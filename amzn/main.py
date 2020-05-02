916f4648b29f0f4ac6d81d30614fe7bdcd9d390f
tamburkey <petem14@gmail.com>
2020-05-01 21:10:22 -0400

    remove line_count

--- amzn/main.py
+++ amzn/main.py
@@ -5,9 +5,7 @@ with open(sys.argv[1]) as csv_in, open('output.csv','w', newline='') as csv_out 
     grades = csv.reader(csv_in)
     grades_writer = csv.writer(csv_out, delimiter=',')
     delete_names = ['Test Student','Stanford Rosenthal', 'Randy Cox', '    Points Possible']
-    line_count = 0
     for row in grades:
-        line_count=line_count+1
         if len(row[-2]) == 2:
             row[-2] = row[-2][0]
         if row[0] not in delete_names:
