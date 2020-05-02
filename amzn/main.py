8f218386c6e071cded36a0383f6d3c496cc6335b
tamburkey <petem14@gmail.com>
2020-05-01 21:08:03 -0400

    remove print statements

--- amzn/main.py
+++ amzn/main.py
@@ -9,8 +9,6 @@ with open(sys.argv[1]) as csv_in, open('output.csv','w', newline='') as csv_out 
     for row in grades:
         line_count=line_count+1
         if len(row[-2]) == 2:
-            print(row[-2])
             row[-2] = row[-2][0]
-            print(row[-2])
         if row[0] not in delete_names:
             grades_writer.writerow(row)
