b0647b3307c417ed993d3303acf86e1fa6f10da3
tamburkey <petem14@gmail.com>
2020-05-01 21:15:46 -0400

    add variables

--- amzn/main.py
+++ amzn/main.py
@@ -5,8 +5,10 @@ with open(sys.argv[1]) as csv_in, open('output.csv','w', newline='') as csv_out 
     grades = csv.reader(csv_in)
     grades_writer = csv.writer(csv_out, delimiter=',')
     delete_names = ['Test Student','Stanford Rosenthal', 'Randy Cox', '    Points Possible']
+    letter_grade = row[-2]
+    student_name = row[0]
     for row in grades:
-        if len(row[-2]) == 2:
-            row[-2] = row[-2][0]
-        if row[0] not in delete_names:
+        if len(letter_grade) == 2:
+            letter_grade = letter_grade[0]
+        if student_name not in delete_names:
             grades_writer.writerow(row)
