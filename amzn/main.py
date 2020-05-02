267ed0ee427974fdcf870830251862eda29309c8
tamburkey <petem14@gmail.com>
2020-05-01 21:21:22 -0400

    move vars

--- amzn/main.py
+++ amzn/main.py
@@ -5,9 +5,9 @@ with open(sys.argv[1]) as csv_in, open('output.csv','w', newline='') as csv_out 
     grades = csv.reader(csv_in)
     grades_writer = csv.writer(csv_out, delimiter=',')
     delete_names = ['Test Student','Stanford Rosenthal', 'Randy Cox', '    Points Possible']
-    letter_grade_field = row[-2]
-    student_name_field = row[0]
     for row in grades:
+        letter_grade_field = row[-2]
+        student_name_field = row[0]
         if len(letter_grade_field) == 2:
             letter_grade_field = letter_grade_field[0]
         if student_name_field not in delete_names:
