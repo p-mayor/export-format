c14dd3253445863dc71c2833fc87322973c9d46e
tamburkey <petem14@gmail.com>
2020-05-03 01:06:55 -0400

    simplify starter / update readme usage

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Format the csv output from the canvas gradebook export.
 
 Add a student name to the delete_names list to remove that row.
+
+Run `python main.py input_file_name.csv` to produce a formated `output.csv` file.
--- q2/main.py
+++ q2/main.py
@@ -6,9 +6,7 @@ with open(sys.argv[1]) as csv_in, open('output.csv','w', newline='') as csv_out 
     grades_writer = csv.writer(csv_out, delimiter=',')
     delete_names = ['Test Student','    Points Possible']
     for row in grades:
-        letter_grade_field = row[-2]
         student_name_field = row[0]
-        if len(letter_grade_field) == 2:
-            letter_grade_field = letter_grade_field[0]
         if student_name_field not in delete_names:
             grades_writer.writerow(row)
+            
